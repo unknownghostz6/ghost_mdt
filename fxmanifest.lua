c7b6b8eef2042e6c7802264dccb6ab1f0f0df8e4
unknownghostz6 <88034715+unknownghostz6@users.noreply.github.com>
2023-03-25 00:59:51 -0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,6 +3,11 @@ fx_version 'adamant'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 ui_page "ui/index.html"
 
+version '2.1'
+vorp_checker 'yes'
+vorp_name '^4Resource version Check^3'
+vorp_github 'https://github.com/unknownghostz6/ghost_mdt'
+
 files {
     "ui/index.html",
     "ui/vue.min.js",
