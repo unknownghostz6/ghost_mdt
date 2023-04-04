3516955253fedadeb11f8594a1fa18ea9ac5b0fc
unknownghostz6 <88034715+unknownghostz6@users.noreply.github.com>
2023-04-03 20:04:54 -0600

    fixed/changed office point coords

--- config.lua
+++ config.lua
@@ -19,37 +19,28 @@ Config.Open = {
 	} 
 Config.Office = {
     [1] = {
-        coords={-304.10, 829.9, 120.0}, -- Valentine
+        coords={-279.33, 808.34, 119.43}, -- Valentine
     },
     [2] = {
-        coords={-325.81, 819.8, 118.0}, -- Valentine 2
-    },
-    [3] = {
         coords={-762.25, -1266.68, 44.15}, -- Blackwater
     },
+    [3] = {
+        coords={-1807.25, -348.27, 164.71}, -- Strawberry Jail Desk
+    },
     [4] = {
-        coords={-1807.1, -348.27, 164.76}, -- Strawberry
+        coords={1362.43, -1300.95, 77.81}, -- Rhodes by Bed
     },
     [5] = {
-        coords={-1810.71, -353.21, 161.54}, -- Strawberry Downstairs
+        coords={2510.12, -1308.78, 49.05}, -- Saint Denis Middle Desk
     },
     [6] = {
-        coords={-325.81, 819.8, 118.0}, -- Rhodes
-    },
-    [7] = {
-        coords={2510.12, -1308.78, 49.05}, -- Saint Denis front desk
-    },
-    [8] = {
-        coords={2495.93, -1306.78, 49.05}, -- Saint Denis Cells
-    },
-    [9] = {
         coords={2907.39, 1313.98, 45.04}, -- Annesburg
     },
-    [10] = {
+    [7] = {
         coords={-3624.39, -2601.61, -13.24}, -- Armadillo
     },
-    [11] = {
-        coords={-5531.15, -2930.18, -1.26}, -- Tumbleweed
+    [8] = {
+        coords={-5530.79, -2930.5, -1.31}, -- Tumbleweed
     },
 }
 
